89c6d744b89b29c82d9cde134f0270267a4edc04
Yuta Ide <sadness.ojisan@gmail.com>
2021-10-17 22:26:10 +0900

    Configure dev env (#1)
    * Run with beta
    
    * success build serenity
    
    * add token
    
    * impl echo
    
    * Update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # discord-kuso-quiz-bot
+
 just quiz bot
+
+```sh
+rustup toolchain install beta
+
+cargo init --edition 2021
+```
+
+Run
+
+```
+cargo +beta run
+```
+
+and open discord.
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+use serenity::async_trait;
+use serenity::client::{Client, Context, EventHandler};
+use serenity::model::channel::Message;
+use serenity::framework::standard::{
+    StandardFramework,
+    CommandResult,
+    macros::{
+        command,
+        group
+    }
+};
+
+use dotenv::dotenv;
+use std::env;
+
+#[group]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            // Sending a message can fail, due to a network error, an
+            // authentication error, or lack of permissions to post in the
+            // channel, so log to stdout when some error happens, with a
+            // description of it.
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv().ok();
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .group(&GENERAL_GROUP);
+
+    // Login with a bot token from the environment
+    let token = env::var("DISCORD_TOKEN").expect("token");
+    let mut client = Client::builder(token)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Error creating client");
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start().await {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
