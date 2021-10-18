17d03f75f38079a6f39ddadbfb961770c4327da8
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-19 03:26:40 +0900

    fix

--- js/index.js
+++ js/index.js
@@ -8,13 +8,17 @@ client.on("ready", () => {
   console.log(`${client.user.tag} でログインしています。`);
 });
 
-const quized = [
-  { title: "test?", answer: "fuga" },
-  { title: "test2?", answer: "fugafuga" },
+const quizSet = [
+  { id: 1, title: "test?", answer: "fuga" },
+  { id: 2, title: "test2?", answer: "fugafuga" },
 ];
+
+const result = {};
 
 let quizing = false;
 let waitingAnswer = false;
+
+let currentQuiz = undefined;
 
 client.on("messageCreate", async (msg) => {
   console.info(`${msg.author.tag}がコマンドを送信しました: ${msg.content}`);
@@ -24,14 +28,37 @@ client.on("messageCreate", async (msg) => {
     quizing = true;
   }
   if (quizing) {
-    if (quized.length === 0 && !waitingAnswer) {
-      msg.channel.send("quizを終了します");
-      quizing = false;
-      return;
-    } else if (quized.length > 0) {
-      msg.channel.send(quized.pop().title);
-      waitingAnswer = true;
-      return;
+    if (waitingAnswer) {
+      const userAnswer = msg.content;
+      result[currentQuiz.id] = currentQuiz.answer === userAnswer;
+      waitingAnswer = false;
+      if (quizSet.length === 0) {
+        msg.channel.send("quizを終了します");
+        console.info(result);
+        msg.channel.send(
+          `${Object.values(result).length}問中${
+            Object.values(result).filter((i) => i).length
+          }問正解`
+        );
+        quizing = false;
+        return;
+      } else if (quizSet.length > 0) {
+        currentQuiz = quizSet.pop();
+        msg.channel.send(currentQuiz.title);
+        waitingAnswer = true;
+        return;
+      }
+    } else {
+      if (quizSet.length === 0) {
+        msg.channel.send("quizを終了します");
+        quizing = false;
+        return;
+      } else if (quizSet.length > 0) {
+        currentQuiz = quizSet.pop();
+        msg.channel.send(currentQuiz.title);
+        waitingAnswer = true;
+        return;
+      }
     }
   }
 });
