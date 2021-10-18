6195943b83b5846d204c340eca4f87f76f1deb1f
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-18 23:54:31 +0900

    start quiz

--- js/index.js
+++ js/index.js
@@ -8,9 +8,24 @@ client.on("ready", () => {
   console.log(`${client.user.tag} でログインしています。`);
 });
 
+const quized = [
+  { title: "test?", answer: "fuga" },
+  { title: "test2?", answer: "fugafuga" },
+];
+
+let quizing = false;
+
 client.on("messageCreate", async (msg) => {
-  if (msg.content === "!ping") {
-    msg.channel.send("Pong!");
+  if (msg.content === "quiz!") {
+    msg.channel.send("quizをスタートします");
+    quizing = true;
+  }
+  if (quizing) {
+    msg.channel.send(quized.pop().title);
+    if (quized.length === 0) {
+      msg.channel.send("quizを終了します");
+      quizing = false;
+    }
   }
 });
 
