0cfcb5a683971cfd1e89824fba46306be2706936
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-18 23:19:30 +0900

    start program

--- js/index.js
+++ js/index.js
@@ -1,36 +1,16 @@
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v9");
-const fs = require("fs");
 require("dotenv").config();
 
 const token = process.env.DISCORD_TOKEN;
+// Require the necessary discord.js classes
+const { Client, Intents } = require("discord.js");
 
-const commands = [];
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+// Create a new client instance
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-// Place your client and guild ids here
-const clientId = "586210162721816587";
-const guildId = "681560227312566362";
+// When the client is ready, run this code (only once)
+client.once("ready", () => {
+  console.log("Ready!");
+});
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: "9" }).setToken(token);
-
-(async () => {
-  try {
-    console.log("Started refreshing application (/) commands.");
-
-    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
-      body: commands,
-    });
-
-    console.log("Successfully reloaded application (/) commands.");
-  } catch (error) {
-    console.error(error);
-  }
-})();
+// Login to Discord with your client's token
+client.login(token);
