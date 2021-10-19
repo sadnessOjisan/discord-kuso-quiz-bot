8c3a22b7a3af73a0bf498655a5ccb5092382a450
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-20 03:05:36 +0900

    Update comment

--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,83 @@
+use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::client::{Client, Context, EventHandler};
-use serenity::model::channel::Message;
 use serenity::framework::standard::{
-    StandardFramework,
-    CommandResult,
-    macros::{
-        command,
-        group
-    }
+    macros::{command, group},
+    CommandResult, StandardFramework,
 };
+use serenity::model::channel::Message;
 use std::collections::HashMap;
-use dotenv::dotenv;
 use std::env;
 
 #[group]
 struct General;
 
 struct Handler {
-    manager: QuizManager
+    manager: QuizManager,
 }
 
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Quiz {
     id: i8,
-    content:String,
-    answer:String
+    content: String,
+    answer: String,
 }
 
 #[derive(Debug)]
 struct QuizManager {
     quizs: Vec<Quiz>,
     currentQuiz: Option<Quiz>,
-    result: HashMap<i8, bool> // {[quizId]: bool}
+    result: HashMap<i8, bool>, // {[quizId]: bool}
+    cursor: Option<i8>,        // TODO: mode 概念を作る
 }
 
 impl QuizManager {
-    fn init(&mut self){
-        self.quizs = vec![
+    fn init(&mut self) {
+        let data = vec![
             Quiz {
                 id: 1,
-                content: "aaa".to_string(),
-                answer: "bbb".to_string()
-            }
-        ]
+                content: "test?".to_string(),
+                answer: "test".to_string(),
+            },
+            Quiz {
+                id: 2,
+                content: "test2?".to_string(),
+                answer: "test2".to_string(),
+            },
+        ];
+        self.quizs = data;
+        self.cursor = Some(0);
+        let quiz = self.quizs[0].clone(); // cloneが無理やりごまかした感がある
+        self.currentQuiz = Some(quiz)
     }
 
+    fn setCurrentQuestion(&mut self, cursor: i8) {}
 }
-
 
 #[async_trait]
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
+        // 質問開始
         if msg.content == "q!" {
-            // Sending a message can fail, due to a network error, an
-            // authentication error, or lack of permissions to post in the
-            // channel, so log to stdout when some error happens, with a
-            // description of it.
-
-            println!("{:?}", &self.manager);
-            
+            println!("{:?}", &self.manager.currentQuiz.as_ref().unwrap());
             if let Err(why) = msg.channel_id.say(&ctx.http, "Quiz を始めます").await {
                 println!("Error sending message: {:?}", why);
             }
+
+            // MEMO: ここの as_ref がなぜ必要か調べる
+            if let Err(why) = msg
+                .channel_id
+                .say(
+                    &ctx.http,
+                    &self.manager.currentQuiz.as_ref().unwrap().content,
+                )
+                .await
+            {
+                println!("Error sending message: {:?}", why);
+            }
         }
+
+        // TODO: 回答を得たときの処理を書く
     }
 }
 
@@ -74,19 +88,19 @@ async fn main() {
         .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
-
     let mut manager = QuizManager {
         quizs: vec![],
         currentQuiz: None,
-        result: HashMap::new()
+        result: HashMap::new(),
+        cursor: None,
     };
-    
+
     manager.init();
 
     // Login with a bot token from the environment
     let token = env::var("DISCORD_TOKEN").expect("token");
     let mut client = Client::builder(token)
-        .event_handler(Handler {manager: manager})
+        .event_handler(Handler { manager: manager })
         .framework(framework)
         .await
         .expect("Error creating client");
