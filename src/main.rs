724af35e611fd81baa16eb6c05da6d243f1b1fba
sadness_ojisan <sadness.ojisan@gmail.com>
2021-10-20 22:24:56 +0900

    wip

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,20 @@ struct Quiz {
     content: String,
     answer: String,
 }
+#[derive(Debug)]
+enum BotState {
+    Initialized,
+    IntentingQuestion,
+    WaitingAnswer,
+}
 
 #[derive(Debug)]
 struct QuizManager {
     quizs: Vec<Quiz>,
     currentQuiz: Option<Quiz>,
     result: HashMap<i8, bool>, // {[quizId]: bool}
-    cursor: Option<i8>,        // TODO: mode 概念を作る
+    cursor: Option<i8>,
+    state: BotState,
 }
 
 impl QuizManager {
@@ -47,37 +54,55 @@ impl QuizManager {
         ];
         self.quizs = data;
         self.cursor = Some(0);
-        let quiz = self.quizs[0].clone(); // cloneが無理やりごまかした感がある
-        self.currentQuiz = Some(quiz)
+        let quiz = self.quizs[self.cursor.unwrap() as usize].clone(); // cloneが無理やりごまかした感がある
+        self.currentQuiz = Some(quiz);
+        self.state = BotState::Initialized;
     }
 
-    fn setCurrentQuestion(&mut self, cursor: i8) {}
+    fn setNextQuestion(&mut self) {
+        // 組み込まれたやり方で Some + Some をやる方法ってないんだっけ？
+        self.cursor = self.cursor.map(|v| v+1); 
+        let quiz = self.quizs[self.cursor.unwrap()as usize].clone(); // cloneが無理やりごまかした感がある
+        self.currentQuiz = Some(quiz);
+        self.state = BotState::WaitingAnswer;
+    }
 }
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&mut self, ctx: Context, msg: Message) {
         // 質問開始
-        if msg.content == "q!" {
-            println!("{:?}", &self.manager.currentQuiz.as_ref().unwrap());
-            if let Err(why) = msg.channel_id.say(&ctx.http, "Quiz を始めます").await {
-                println!("Error sending message: {:?}", why);
-            }
+        match &self.manager.state {
+            BotState::Initialized => {
+                if msg.content == "q!" {
+                    println!("{:?}", &self.manager.currentQuiz.as_ref().unwrap());
+                    if let Err(why) = msg.channel_id.say(&ctx.http, "Quiz を始めます").await {
+                        println!("Error sending message: {:?}", why);
+                    }
 
-            // MEMO: ここの as_ref がなぜ必要か調べる
-            if let Err(why) = msg
-                .channel_id
-                .say(
-                    &ctx.http,
-                    &self.manager.currentQuiz.as_ref().unwrap().content,
-                )
-                .await
-            {
-                println!("Error sending message: {:?}", why);
-            }
-        }
+                    // MEMO: ここの as_ref がなぜ必要か調べる
+                    if let Err(why) = msg
+                        .channel_id
+                        .say(
+                            &ctx.http,
+                            &self.manager.currentQuiz.as_ref().unwrap().content,
+                        )
+                        .await
+                    {
+                        println!("Error sending message: {:?}", why);
+                    }
 
-        // TODO: 回答を得たときの処理を書く
+                    &self.manager.setNextQuestion();
+                }
+            }
+            BotState::IntentingQuestion => {}
+            BotState::WaitingAnswer => {
+                let userAnswer = msg.content;
+                let currentQuestion = &self.manager.currentQuiz;
+                let currentQuestionAnswer = currentQuestion.as_ref().unwrap().answer;
+                &self.manager.result.insert(currentQuestion.unwrap().id, userAnswer == currentQuestionAnswer);
+            }
+        };
     }
 }
 
@@ -93,6 +118,7 @@ async fn main() {
         currentQuiz: None,
         result: HashMap::new(),
         cursor: None,
+        state: BotState::Initialized,
     };
 
     manager.init();
