87fb6b4babaffb5c182c92e2b2f9a1765a2a42a4
Yuta Ide <sadness.ojisan@gmail.com>
2021-11-01 01:18:26 +0900

    Homework(2021/10/24~) (#11)
    * to create pr
    
    * refactoring (#12)
    
    * resolve error
    
    * stop fail
    
    * Return is_correct
    
    * return question
    
    * Return result
    
    * 複数チャンネル対応 (#13)
    
    * wip
    
    * wip
    
    * memo
    
    * messaging fail
    
    * wip
    
    * wip
    
    * wip
    
    * success
    
    * リファクタリング (#14)
    
    * create quiz mod
    
    * Add expect
    
    * fmt

--- README.md
+++ README.md
@@ -15,3 +15,22 @@ cargo +beta run
 ```
 
 and open discord.
+
+## 質問
+
+### tokio そのものってマルチスレッド + event loop の合わせ技ってこと？
+
+### dereference と Copy の関係
+
+ごまかそうとして dereference しようとすると Copy を要求される。String が混じっているとできない。
+
+### :: の意味がよく分からない
+
+module を辿っているのはわかるがジェネリクスの指定もそうなっていて、そういうものか？と言う気持ち
+
+- ` .type_map_insert::<BotState>(Arc::new(Mutex::new(initial_state)))`
+- ` let (tx, mut rx) = mpsc::channel::<String>(32);`
+
+## memo
+
+-
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,16 @@
+use discord_kuso_quiz_bot::quiz::quiz::{AllBotState, BotState, Mode};
 use dotenv::dotenv;
 use serenity::async_trait;
 use serenity::framework::standard::{
     macros::{command, group},
     Args, CommandResult, StandardFramework,
 };
+use serenity::futures::lock::Mutex;
 use serenity::model::{channel::Message, gateway::Ready};
 use serenity::prelude::*;
-use std::collections::HashMap;
 use std::sync::Arc;
-use std::vec;
 
 struct Handler;
-
-type QuestionID = i8;
-#[derive(Debug, Clone)]
-struct Question {
-    id: QuestionID,
-    content: String,
-    answer: String,
-}
-#[derive(Debug, Clone)]
-enum Mode {
-    WaitingUserAnswer,
-    Questioning,
-}
-#[derive(Debug, Clone)]
-struct State {
-    questions: Vec<Question>,
-    result: HashMap<QuestionID, bool>,
-    cursor: Option<u8>,
-    mode: Mode,
-}
-
-impl State {
-    fn init(&mut self) {
-        // & なくてよい？
-        self.questions = vec![
-            Question {
-                id: 1,
-                content: "test".to_string(),
-                answer: "hoge".to_string(),
-            },
-            Question {
-                id: 2,
-                content: "test2".to_string(),
-                answer: "hoge2".to_string(),
-            },
-        ];
-        self.cursor = Some(0);
-        self.mode = Mode::Questioning
-    }
-
-    fn next_question(&mut self) {
-        self.cursor = self.cursor.map(|v| v + 1);
-        self.mode = Mode::WaitingUserAnswer
-    }
-}
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -64,24 +19,77 @@ impl EventHandler for Handler {
     }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        let mut data = ctx.data.write().await;
-        let state = data.get_mut::<State>().expect("Failed to retrieve map!");
-        let current_state = &mut state.lock().await;
+        if msg.author.name == "kuso-quiz" {
+            return;
+        }
 
+        let channel_id = msg.channel_id;
+        let mut data = ctx.data.write().await;
+        let mut state = data
+            .get_mut::<AllBotState>()
+            .expect("Failed to retrieve map!")
+            .lock()
+            .await;
+
+        let current_state = state.states.get_mut(&channel_id).unwrap();
         match &current_state.mode {
-            Mode::Questioning => {}
-            Mode::WaitingUserAnswer => {
-                let cursor = &current_state.cursor;
-                match cursor {
-                    None => {}
-                    Some(v) => {
-                        let current_q = &current_state.questions[*v as usize].clone();
-                        let current_q_answer = &current_q.answer; // どうしてこれは参照がいるのか
-                        let user_answer = msg.content;
-                        let result = current_q_answer.as_ref() == user_answer;
-                        current_state.result.insert(current_q.id, result);
+            Mode::WaitingUserAnswer(_) => {
+                let user_answer = msg.content;
+                let is_correct = current_state.user_answer(user_answer);
+                // Q: current_state.next_quiz(); user_answer を next_quiz() から呼ぶようにしたのでエラーを回避できたけど、本当にこれでいいのか？
+                if is_correct.is_none() {
+                    msg.channel_id
+                        .say(&ctx.http, "不正な状態です。")
+                        .await
+                        .expect("fail send message");
+                    return;
+                }
+                if is_correct.unwrap() {
+                    msg.channel_id
+                        .say(&ctx.http, "正解です。")
+                        .await
+                        .expect("fail send message");
+                } else {
+                    msg.channel_id
+                        .say(&ctx.http, "不正解です。")
+                        .await
+                        .expect("fail send message");
+                }
+                // let next_state = &mut bot_state.lock().await;
+                // println!("next_state{:?}",next_state); 上でlockをとろうとするとここでコードが止まる。下にcurrent_state(=&mut bot_state.lock().await;)がいてライフタイムがあるからロックが取れないのだと思うけど、そういう競合のときってエラーとかで検知できないのか？try_lockはこういうときのためのもの？
+                match &current_state.mode {
+                    Mode::WaitingUserAnswer(state) => {
+                        let current_quiz = &state.questions[state.cursor as usize].clone();
+                        let res = msg.channel_id.say(&ctx.http, &current_quiz.content).await;
+                        if res.is_err() {
+                            println!("不正な状態です")
+                        }
+                    }
+                    Mode::Finish(state) => {
+                        let all_q_lens = &state.questions.len();
+                        let correct_list = &state.result.len();
+                        let txt = format!("{:?}問中{:?}正解です", all_q_lens, correct_list);
+                        msg.channel_id
+                            .say(&ctx.http, txt)
+                            .await
+                            .expect("fail send message");
+                    }
+                    _ => {
+                        msg.channel_id
+                            .say(&ctx.http, "不正な状態です。")
+                            .await
+                            .expect("fail send message");
                     }
                 }
+            }
+            Mode::Error => {
+                msg.channel_id
+                    .say(&ctx.http, "回答待ちではありません")
+                    .await
+                    .expect("fail send message");
+            }
+            _ => {
+                println!("fail")
             }
         }
     }
@@ -91,34 +99,40 @@ impl EventHandler for Handler {
 #[commands(start)]
 struct General;
 
-impl TypeMapKey for State {
-    type Value = Arc<Mutex<State>>;
-}
-
 #[command]
 async fn start(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResult {
+    let channel_id = msg.channel_id;
     let mut data = ctx.data.write().await;
-    let state = data.get_mut::<State>().expect("Failed to retrieve map!");
-    state.lock().await.init();
-    println!("{:?}", &state.lock().await.cursor);
+    let mut state = data
+        .get_mut::<AllBotState>()
+        .expect("Failed to retrieve map!")
+        .lock()
+        .await;
+    let bot_state = state.states.get(&channel_id);
+    if bot_state.is_none() {
+        let mut initial_state = BotState { mode: Mode::Init };
+        initial_state.initialize_quiz();
+        state.states.insert(channel_id, initial_state);
+    };
+
+    let bot_state = state.states.get(&channel_id).unwrap();
     msg.channel_id.say(&ctx.http, "Quiz を始めます。").await?;
-    state.lock().await.next_question();
-    let current_state = &state.lock().await;
-    match &current_state.cursor {
-        None => {
-            msg.channel_id
-                .say(&ctx.http, "初期化されていません")
-                .await?;
+    let current_state = bot_state;
+    match &current_state.mode {
+        Mode::WaitingUserAnswer(state) => {
+            let cursor = &state.cursor;
+            let current_question = &state.questions[*cursor as usize];
+            let quiz = &current_question.content;
+            msg.channel_id.say(&ctx.http, quiz).await?;
+            Ok(())
         }
-        Some(v) => {
-            let current_question = &current_state.questions[*v as usize];
-            let txt = &current_question.content;
-            println!("{:?}", &current_question);
-            msg.channel_id.say(&ctx.http, txt).await?;
+        _ => {
+            msg.channel_id
+                .say(&ctx.http, "クイズの初期化に失敗しました。")
+                .await?;
+            Ok(()) // Q: Error を返したい
         }
     }
-    // println!("{:?}", &map.lock().await.cursor);
-    Ok(())
 }
 
 #[tokio::main]
@@ -130,17 +144,12 @@ async fn main() {
         .configure(|c| c.case_insensitivity(true))
         .group(&GENERAL_GROUP);
 
-    let initial_state = State {
-        questions: vec![],
-        result: HashMap::new(),
-        cursor: None,
-        mode: Mode::Questioning,
-    };
+    let initial_state = AllBotState::new();
 
     let mut client = Client::builder(&token)
         .event_handler(Handler)
         .framework(framework)
-        .type_map_insert::<State>(Arc::new(Mutex::new(initial_state))) // new!
+        .type_map_insert::<AllBotState>(Arc::new(Mutex::new(initial_state))) // new!
         .await
         .expect("Failed to build client");
 
