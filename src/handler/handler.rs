5146bc0c637fc73d4f8b027b727302d415c0629f
sadness_ojisan <sadness.ojisan@gmail.com>
2021-11-02 02:37:08 +0900

    wip

--- src/handler/handler.rs
+++ src/handler/handler.rs
@@ -1,5 +1,6 @@
 use serenity::async_trait;
 use serenity::client::{Context, EventHandler};
+use serenity::model::channel::Message;
 use serenity::model::prelude::Ready;
 
 pub struct Handler;
@@ -8,5 +9,15 @@ pub struct Handler;
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("Bot ready with username {}", ready.user.name);
+    }
+    
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.author.name == "kuso-quiz" {
+            return;
+        }
+        tokio::spawn(||{
+            
+        });
+        if msg.content == "start" {}
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 use discord_kuso_quiz_bot::handler::Handler;
-use discord_kuso_quiz_bot::quiz::{AllBotState, Mode};
 use dotenv::dotenv;
-use serenity::async_trait;
-use serenity::futures::lock::Mutex;
-use serenity::model::{channel::Message, gateway::Ready};
 use serenity::prelude::*;
-use std::sync::Arc;
 
 #[tokio::main]
 async fn main() {
