e694fe47942af66c9b1db1ebff35a0d5ba7086d0
sadness_ojisan <sadness.ojisan@gmail.com>
2021-11-05 01:18:09 +0900

    send bot state

--- src/handler/handler.rs
+++ src/handler/handler.rs
@@ -9,6 +9,7 @@ use serenity::model::prelude::Ready;
 use tokio::sync::mpsc;
 
 use crate::bot::BotState;
+use crate::quiz::State;
 
 pub struct Handler {
     pub channel_sender_pair: HashMap<ChannelId, Sender<String>>,
@@ -18,16 +19,15 @@ pub struct Handler {
 impl EventHandler for Handler {
     async fn ready(&self, _: Context, ready: Ready) {
         println!("Bot ready with username {}", ready.user.name);
-    }   
+    }
 
     async fn message(&self, ctx: Context, msg: Message) {
-        use serenity::futures::SinkExt;
         if msg.author.name == "kuso-quiz" {
             return;
         }
         if msg.content == "start" {
             let channel_id = msg.channel_id;
-            let mut rw_lock= ctx.data.write().await;
+            let mut rw_lock = ctx.data.write().await;
             let mut bot_state = rw_lock
                 .get_mut::<BotState>()
                 .expect("Failed to retrieve map!")
@@ -37,17 +37,32 @@ impl EventHandler for Handler {
             if target_sender.is_some() {
                 // すでに登録済み
                 let sender = target_sender.unwrap();
-             let _ =  sender.send("hello from sender".to_string()).await;
-                // TODO: send message
+                let _ = sender.send("hello from sender".to_string()).await;
             } else {
                 // まだ登録していない
-                let (tx,mut rx) = mpsc::channel(32);
+                let (tx, mut rx) = mpsc::channel(32);
                 bot_state.channel_sender_pair.insert(channel_id, tx);
                 tokio::spawn(async move {
+                    let mut state = State::new();
+                    let question = state.get_current_question();
+                  let res =  msg.channel_id.say(&ctx.http, &question.content).await;
+                  println!("{:?}", res);
                     loop {
-                        // TODO: receive message
-                        let msg = rx.recv().await;
-                        println!("received: {:?}", msg);
+                        let user_message = rx.recv().await.expect("fail to receive message");
+                        let is_correct = state.check_user_answer(&user_message);
+                        if is_correct {
+                            let _ = msg.channel_id.say(&ctx.http, "正解です。").await;
+                        } else {
+                            let _ = msg.channel_id.say(&ctx.http, "不正解です。").await;
+                        }
+                        if state.is_last() {
+                            let result_summary = state.summary_result();
+                            let result_message =
+                                format!("{}問中{}問正解です。", result_summary.0, result_summary.1);
+                            let _ = msg.channel_id.say(&ctx.http, result_message).await;
+                        } else {
+                            state.next_question()
+                        }
                     }
                 });
             };
